1ce8fe6365a2b544af2a6a1c5ceb93f8ed55843d
Misagh <misagh.moayyed@gmail.com>
2021-05-09 16:08:58 +0400

    dark mode after hours

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,7 +5,13 @@ $(function () {
 
 function switchMode() {
   $('body,div.card')
-    .css("background-color", "#0d0c0c")
+    .css("background-color", "#020001")
+    .css("background-image", "linear-gradient(to right, #020001, #1a1618)")
     .css("color", "#d6d6d6")
   $('code.highlighter-rouge').css("color", "#dfcb66");
+}
+
+let hour = new Date().getHours();
+if (hour > 18) {
+  switchMode();
 }
