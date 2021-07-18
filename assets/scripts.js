ed9cc7f45df84f6111554bac8e2e5d25eebc1e0a
Misagh <mm1844@gmail.com>
2021-07-18 13:40:11 +0400

    add copy button

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,3 +11,21 @@ function switchMode() {
   $('code.highlighter-rouge').css("color", "#dfcb66");
 }
 
+let codes = document.querySelectorAll('.highlight > pre > code');
+let countID = 0;
+codes.forEach((code) => {
+  code.setAttribute("id", "code" + countID);
+  let btn = document.createElement('button');
+  btn.innerHTML = "Copy";
+  btn.className = "btn-copy-code";
+  btn.setAttribute("data-clipboard-action", "copy");
+  btn.setAttribute("data-clipboard-target", "#code" + countID);
+  
+  let div = document.createElement('div');
+  div.className = "div-code-button";
+  div.appendChild(btn);
+  
+  code.before(div);
+  countID++;
+}); 
+new ClipboardJS('.btn-copy-code');
