ba9a2f893a696b17a0fe1474b8bf38c1bb6a9250
Misagh Moayyed <mm1844@gmail.com>
2023-12-21 10:40:33 +0400

    new design

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,19 +2,6 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 })
 
-
-function switchMode() {
-  $('body')
-    .css("background-color", "#030a0d")
-    // .css("background-image", "linear-gradient(to right, #020001, #030a0d)")
-    .css("color", "#d6d6d6");
-  $('code.highlighter-rouge').css("color", "#dfcb66");
-  $('blockquote,h1,h2,h3,h4,h5,h6,h5>a').css("color", "ghostwhite");
-  $('#contact-section h6').css("color", "lightyellow");
-
-  $('div.card').css('background-image', 'linear-gradient(to right, rgb(21 32 40), rgb(29 61 76))');
-}
-
 let codes = document.querySelectorAll('.highlight > pre > code');
 let countID = 0;
 codes.forEach((code) => {
@@ -33,8 +20,6 @@ codes.forEach((code) => {
   countID++;
 }); 
 new ClipboardJS('.btn-copy-code');
-switchMode();
-
 setTimeout(function () {
   $(".masthead").css("filter", "grayscale(0)")
-}, 1000);
+}, 900);
